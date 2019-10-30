6db2b4e81418a309bd31ce79a1d2915dce937c36
5G 19-20 <5G 19-20@PC3-Info1>
2019-10-30 13:47:47 +0100

    versione con anche il dizionario

--- PrimoDB/src/main/java/it/gov/itisfeltrinelli/PrimoDB/App.java
+++ PrimoDB/src/main/java/it/gov/itisfeltrinelli/PrimoDB/App.java
@@ -9,15 +9,17 @@ import java.sql.Statement;
 public class App {
 
 	public static void main(String[] args) {
-		String DBurl = "jdbc:mysql://localhost:3306/perinialves";
+//		String DBurl = "jdbc:mysql://localhost:3306/perinialves";
+		String DBurl = "jdbc:mysql://localhost:3306/dizionario";
 		try {
 			Connection c = DriverManager.getConnection(DBurl, "root", "");
-			String stmt = " SELECT nome, cognome, eta from primodb;";
+			String stmt = " SELECT * FROM parola WHERE nome='v%';";
 			Statement s = c.createStatement();
 			ResultSet r = s.executeQuery(stmt);
 			while (r.next()) {
-				System.out.format("Nome: \"%s\" Cognome \"%s\" Eta %d anni \n", r.getString("NOME"),
-						r.getString("COGNOME"), r.getInt("ETA"));
+//				System.out.format("Nome: \"%s\" Cognome \"%s\" Eta %d anni \n", r.getString("NOME"),
+//						r.getString("COGNOME"), r.getInt("ETA"));
+				System.out.format("ID: %d \"%s\" \n", r.getInt("ID"), r.getString("NOME"));
 
 			}
 			if (null != r)
